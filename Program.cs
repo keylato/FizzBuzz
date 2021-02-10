a7d7288dc068534e29eb1cea35f374083432021b
Keyla <keylato96@gmail.com>
2021-02-10 11:59:22 -0700

    se suben archivos creados

--- Program.cs
+++ Program.cs
@@ -6,7 +6,35 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int [] numeros = new int [100];
+         
+
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                numeros [i]= i+i;
+            }
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                if ((numeros [i] % 3) ==0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if ((numeros [i] %3 ==0) && (numeros [i] % 5 ==0))
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if ((numeros [i] % 5) ==0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else 
+                {
+                    Console.WriteLine(numeros [i]);
+                }
+            }
+            
+         
+         
         }
     }
 }
