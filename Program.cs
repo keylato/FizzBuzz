8c0e053d69909e84ebf352f3241f4ea70060e16e
keylato <78444133+keylato@users.noreply.github.com>
2021-03-03 10:56:47 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] numeros = new int [100];
+         
+
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                numeros [i]= i+i;
+            }
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                if ((numeros [i] % 5 ==0))
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else if ((numeros[i] %3 ==0 && numeros[i] % 5 ==0))
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if ((numeros [i] % 3 ==0))
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else 
+                {
+                    Console.WriteLine(numeros [i]);
+                }
+                
+            }
+            
+         
+         
+        }
+    }
+}
